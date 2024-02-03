37b2e96820b6ad7d6c59571881aac11fa9e3df4b
Fefedu973 <80718477+Fefedu973@users.noreply.github.com>
2024-02-03 11:36:45 +0100

    first files

---
+++ OpenRGBBridge.js
@@ -0,0 +1,55 @@
+export function Name() { return ""; }
+export function VendorId() { return ; }
+export function ProductId() { return ; }
+export function Publisher() { return ""; }
+export function Size() { return [1,1]; }
+export function DefaultPosition(){return [10, 100]; }
+export function DefaultScale(){return 8.0}
+export function ControllableParameters() {
+	return [
+		{"property":"shutdownColor", "group":"lighting", "label":"Shutdown Color", "min":"0", "max":"360", "type":"color", "default":"009bde"},
+		{"property":"LightingMode", "group":"lighting", "label":"Lighting Mode", "type":"combobox", "values":["Canvas", "Forced"], "default":"Canvas"},
+		{"property":"forcedColor", "group":"lighting", "label":"Forced Color", "min":"0", "max":"360", "type":"color", "default":"009bde"},
+	];
+}
+
+export function Initialize() {
+
+}
+
+var vLedNames = [ "Led 1" ]; 
+var vLedPositions = [ [0,0] ];
+
+export function LedNames() {
+
+}
+
+export function LedPositions() {
+
+}
+
+export function Render() {
+
+}
+
+export function Shutdown() {
+
+}
+
+function hexToRgb(hex) {
+	let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	let colors = [];
+	colors[0] = parseInt(result[1], 16);
+	colors[1] = parseInt(result[2], 16);
+	colors[2] = parseInt(result[3], 16);
+
+	return colors;
+}
+
+export function Validate(endpoint) {
+	return endpoint.interface === 0 && endpoint.usage === 0 && endpoint.usage_page === 0;
+}
+
+export function Image() {
+	return "";
+}
---
+++ OpenRGBBridge.qml
@@ -0,0 +1,11 @@
+Rectangle {
+    width: 200
+    height: 200
+    color: "lightblue"
+
+    Text {
+        text: "Hello"
+        anchors.centerIn: parent
+        font.pixelSize: 24
+    }
+}
